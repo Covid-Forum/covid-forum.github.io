cfd21709b1acfb0bc716c13a4a6f217713692d4b
vfon98 <vfon98@gmail.com>
2020-06-04 14:20:47 +0700

    Release 1.3

---
+++ precache-manifest.3e74217184fb6990f0248cea7c5af581.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e5acc8282dc5335ade982c197b5fe8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed011b73b240bf1882e5",
+    "url": "./static/css/2.1eb21bb0.chunk.css"
+  },
+  {
+    "revision": "511cb63f09af24dc4bb1",
+    "url": "./static/css/main.66da5bd5.chunk.css"
+  },
+  {
+    "revision": "ed011b73b240bf1882e5",
+    "url": "./static/js/2.55bf8d38.chunk.js"
+  },
+  {
+    "revision": "59ecdbee365219334be02f18bcc94c8d",
+    "url": "./static/js/2.55bf8d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "511cb63f09af24dc4bb1",
+    "url": "./static/js/main.84137aaa.chunk.js"
+  },
+  {
+    "revision": "67cb0986dff4396fe2b5",
+    "url": "./static/js/runtime-main.90f289fb.js"
+  },
+  {
+    "revision": "0c77c6bb83c2baafb842ac059cbff121",
+    "url": "./static/media/android.0c77c6bb.jpg"
+  },
+  {
+    "revision": "8d186cb4c6ebf3aa02831420be820314",
+    "url": "./static/media/android.8d186cb4.jpg"
+  },
+  {
+    "revision": "55789da292974620b4d3cea8de2f1c67",
+    "url": "./static/media/background.55789da2.jpg"
+  },
+  {
+    "revision": "8ee4568395b6e57d7417d410f9d2a233",
+    "url": "./static/media/ios.8ee45683.jpg"
+  },
+  {
+    "revision": "a88eb22b30400dff2d73270789d2a018",
+    "url": "./static/media/ios.a88eb22b.jpg"
+  },
+  {
+    "revision": "0f47f5ad7195565d98ca5fbe5b099a8f",
+    "url": "./static/media/laptop_desktop.0f47f5ad.jpg"
+  },
+  {
+    "revision": "0b02e6c4e082ea1b49b353c3e1739517",
+    "url": "./static/media/linux.0b02e6c4.jpg"
+  },
+  {
+    "revision": "8e545ce8f0036f28ae4a78878c2d34ed",
+    "url": "./static/media/macos.8e545ce8.jpg"
+  },
+  {
+    "revision": "a30d51a01949d4fffed67f5b8e1cb112",
+    "url": "./static/media/macos.a30d51a0.jpg"
+  },
+  {
+    "revision": "099e9baea81385ece10695dc1efd249e",
+    "url": "./static/media/news-placeholder.099e9bae.jpg"
+  },
+  {
+    "revision": "b5cb9e6c9a93f81fb3493f0f3fbe0b3a",
+    "url": "./static/media/news_1.b5cb9e6c.jpg"
+  },
+  {
+    "revision": "f5f106269510fe7e9bc54aaf507a5637",
+    "url": "./static/media/news_10.f5f10626.jpg"
+  },
+  {
+    "revision": "0d47457b4b3ac610d211ee1edd3fb956",
+    "url": "./static/media/news_2.0d47457b.jpg"
+  },
+  {
+    "revision": "d2754ca5f334e8b5fd1712b263b08ae4",
+    "url": "./static/media/news_3.d2754ca5.jpg"
+  },
+  {
+    "revision": "8458a8d9c8b6a73f653ae434cde97c36",
+    "url": "./static/media/news_4.8458a8d9.jpg"
+  },
+  {
+    "revision": "5a6bce51c8bf401fb970b7d3c6f38b96",
+    "url": "./static/media/news_5.5a6bce51.jpg"
+  },
+  {
+    "revision": "82d9b7cd4021af40763364aef7d2dac1",
+    "url": "./static/media/news_6.82d9b7cd.jpg"
+  },
+  {
+    "revision": "56de81c6f228f59af9594ca418b606ef",
+    "url": "./static/media/news_7.56de81c6.jpg"
+  },
+  {
+    "revision": "fc125f5b74ffcc4f7f12d10fe016deae",
+    "url": "./static/media/news_8.fc125f5b.jpg"
+  },
+  {
+    "revision": "dc1bb8e68f8565c43bd21155388ef105",
+    "url": "./static/media/news_9.dc1bb8e6.jpg"
+  },
+  {
+    "revision": "e1fa6b82ad9352fa8eba6d7661d32825",
+    "url": "./static/media/programing.e1fa6b82.jpg"
+  },
+  {
+    "revision": "dfeecb236462a303088b97c93f2c0e13",
+    "url": "./static/media/smartphone.dfeecb23.jpg"
+  },
+  {
+    "revision": "34337b30533b05691993b56b13b6e551",
+    "url": "./static/media/windows.34337b30.jpg"
+  },
+  {
+    "revision": "b8dcd2fe6d26149d49d27aece349c9f9",
+    "url": "./static/media/windows.b8dcd2fe.jpg"
+  }
+]);
